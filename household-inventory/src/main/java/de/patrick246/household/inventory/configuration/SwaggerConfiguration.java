b6c34d051b34fb73f5eb66e8207ce9fffca13637
Patrick Hahn <patricksilashahn@gmx.net>
2019-10-31 20:13:28 +0100

    add spring security openid add swagger security configuration

--- household-inventory/src/main/java/de/patrick246/household/inventory/configuration/SwaggerConfiguration.java
+++ household-inventory/src/main/java/de/patrick246/household/inventory/configuration/SwaggerConfiguration.java
@@ -1,16 +1,20 @@
 package de.patrick246.household.inventory.configuration;
 
+import com.google.common.net.HttpHeaders;
+import io.swagger.models.auth.In;
 import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -25,7 +29,7 @@ public class SwaggerConfiguration {
                 .pathMapping("/")
                 .directModelSubstitute(LocalDate.class, String.class)
                 .directModelSubstitute(ObjectId.class, String.class)
-                .genericModelSubstitutes(ResponseEntity.class);
-
+                .genericModelSubstitutes(ResponseEntity.class)
+                .securitySchemes(Collections.singletonList(new ApiKey("OAuth2 Bearer Token", HttpHeaders.AUTHORIZATION, In.HEADER.name())));
     }
 }
